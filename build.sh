e05e6e0fa5b44c1984307fcb12e55cb23a183f83
JhonJerez <jhonx3099@gmail.com>
2025-12-10 01:08:54 -0500

    oo

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 set -o errexit
 pip install -r requirements.txt
 
-# CREAR OBJETO GYM Y ASIGNAR EL OWNER (LÓGICA IF EXPLÍCITA)
-python manage.py shell -c "from django.contrib.auth import get_user_model; from gyms.models import Gym; User = get_user_model(); owner = User.objects.get(email='owner@gimnasio.com'); if not Gym.objects.filter(owner=owner).exists(): Gym.objects.create(name='Gimnasio Principal', owner=owner)"
+# CREAR OBJETO GYM USANDO get_or_create()
+python manage.py shell -c "from django.contrib.auth import get_user_model; from gyms.models import Gym; User = get_user_model(); owner = User.objects.get(email='owner@gimnasio.com'); Gym.objects.get_or_create(owner=owner, defaults={'name': 'SpartaGym', 'address': '123 Fitness St', 'city': 'Fitville', 'state': 'CA', 'zip_code': '90001'})"
 
 python manage.py collectstatic --no-input
 python manage.py migrate
