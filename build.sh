a9f5bf807ce9e06e1fce7285eb91a6712b3c23dc
JhonJerez <jhonx3099@gmail.com>
2025-12-10 01:03:11 -0500

    Refactor: Actualizar el script de construcción para crear el gimnasio 'Gimnasio Principal'

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 set -o errexit
 pip install -r requirements.txt
 
-# 3. CREAR OBJETO GYM Y ASIGNAR EL OWNER
-python manage.py shell -c "from django.contrib.auth import get_user_model; from gyms.models import Gym; User = get_user_model(); owner = User.objects.get(email='owner@gimnasio.com'); Gym.objects.filter(name='SpartanosGym').exists() or Gym.objects.create(name='SpartanosGym', owner=owner)"
+# LIMPIA el build.sh para que solo contenga los comandos SEGUROS
+
+# 1. Comando de creación del GYM (¡Asegúrate de que esta línea esté puesta ANTES de migrate!)
+python manage.py shell -c "from django.contrib.auth import get_user_model; from gyms.models import Gym; User = get_user_model(); owner = User.objects.get(email='owner@gimnasio.com'); Gym.objects.filter(name='Gimnasio Principal').exists() or Gym.objects.create(name='Gimnasio Principal', owner=owner)"
 
 python manage.py collectstatic --no-input
 python manage.py migrate
