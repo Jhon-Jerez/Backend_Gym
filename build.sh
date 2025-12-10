ad29ac5f92242af8165fc2ad7e3bc2936ee9120f
JhonJerez <jhonx3099@gmail.com>
2025-12-10 00:48:58 -0500

    "Deployment ready: Initial migrations for core, gyms, and calendario created and dependencies resolved."

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
-
-pip install -r requirements.txt # Primero, instala TODO.
-
-python manage.py migrate core
-
-# 2. Luego aplica todas las demás migraciones, usando --fake-initial para evitar que Django
-# entre en conflicto con las dependencias de 'auth'.
-python manage.py migrate --fake-initial
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
