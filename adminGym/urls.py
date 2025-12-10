bb60970d12bfdd16f11b32ffd2e15aeeb7772a3c
JhonJerez <jhonx3099@gmail.com>
2025-12-09 23:55:32 -0500

    cambios

--- adminGym/urls.py
+++ adminGym/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import HttpResponse from django.http
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
@@ -28,6 +29,7 @@ urlpatterns = [
     path('api/core/', include('core.urls')),
     path("api/gyms/", include("gyms.urls")),
     path("api/calendario/", include("calendario.urls")),
+    path('health/', lambda r: HttpResponse('ok'), name='health_check')
 
 
 ]
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+pip install -r requirements.txt # Primero, instala TODO.
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py collectstatic --no-input # Luego, recoge estáticos.
+python manage.py migrate # Finalmente, migra la base de datos.
