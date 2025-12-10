b67504e61330d33cfc1a6823e57452b41e28976d
JhonJerez <jhonx3099@gmail.com>
2025-12-10 09:18:43 -0500

    Feat: Añadir dominios permitidos para CORS en la configuración de Django

--- adminGym/settings.py
+++ adminGym/settings.py
@@ -171,10 +171,12 @@ if not DEBUG:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    "https://backend-gym-apii.onrender.com"
     "http://localhost:5173", 
     "http://localhost:3000", 
     "http://127.0.0.1:5173",
     "http://127.0.0.1:3000"
+    "https://frontend-gym-j294.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
